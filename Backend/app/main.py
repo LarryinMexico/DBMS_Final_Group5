287835dc70c3907fd6dcbb3b5216a6cfbd4846a3
chuang091 <thomas@ultralytics.com>
2025-04-04 15:06:11 +0800

    chore(BE): CD

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers import user
+import uvicorn
 
 app = FastAPI()
 app.include_router(user.router, prefix="/users", tags=["Users"])
@@ -8,3 +9,6 @@ app.include_router(user.router, prefix="/users", tags=["Users"])
 @app.get("/")
 def root():
     return {"message": "Welcome to Toilet Map API 🧻"}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080)
