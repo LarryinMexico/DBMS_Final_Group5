6135d4f3927937035b986787a802778aa211b578
YuChenLin <linrain0323@gmail.com>
2025-04-22 22:26:32 +0800

    edit import

--- Backend/app/routers/favorite.py
+++ Backend/app/routers/favorite.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 from app.CRUD import favorite as crud_favorite
 from app.schemas import favorite as favorite_schema
 from app.db.session import get_db
-from typing import List
 
 router = APIRouter()
 
