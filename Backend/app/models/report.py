be413280adb313d408f49e350b90ec023f31380f
YuChenLin <linrain0323@gmail.com>
2025-04-26 15:47:10 +0800

    feat(model): Add status attribute to Report

--- Backend/app/models/report.py
+++ Backend/app/models/report.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer,String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.db.base import Base
-
 class Report(Base):
     __tablename__ = "report"
     
@@ -9,10 +8,10 @@ class Report(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     toilet_id = Column(Integer, ForeignKey("toilet.id"), nullable=False)
     description = Column(String(255), nullable=False)
+    status = Column(String(20), nullable=False, default="pending") # 狀態可以是 "pending", "resolved", "rejected" 
 
     # 建立與User的多對一關係
     user = relationship("User", back_populates="reports")
 
     # 建立與User的多對一關係
     toilet = relationship("Toilet", back_populates="reports")
-
