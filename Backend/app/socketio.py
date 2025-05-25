68a4db111ca04c3579c4b1fb575142f7ae3962c8
chuang091 <thomas@ultralytics.com>
2025-05-25 16:50:19 +0800

    fix: cors

--- Backend/app/socketio.py
+++ Backend/app/socketio.py
@@ -2,7 +2,8 @@ import socketio
 
 sio = socketio.AsyncServer(
     async_mode="asgi",
-    cors_allowed_origins=["*"]
+    cors_allowed_origins="*",
+    ping_interval=25,  # 每 25 秒發 ping，讓 Cloud Run 不殺連線
+    ping_timeout=60,
 )
-
 
